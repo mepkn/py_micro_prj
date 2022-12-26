21de3d089c56e27b79bd7a8a73a7f9e69f7834ec
Praveen Kumar Nirmalkar <iamthepkn@gmail.com>
2022-12-26 16:51:26 +0530

    Create count_info.py

---
+++ count_info.py
@@ -0,0 +1,26 @@
+def count_info(string: str) -> tuple:
+    lines = 0
+    words = 0
+    vowels = 0
+    consonants = 0
+
+    lines_list = string.splitlines()
+    for line in lines_list:
+        lines += 1
+        words_list = line.split()
+        for word in words_list:
+            for char in word:
+                if char in "aeiouAEIOU":
+                    vowels += 1
+                else:
+                    consonants += 1
+        words += len(words_list)
+
+    return lines, words, vowels, consonants
+
+
+# Test the function
+string = """This is a string
+with multiple lines
+and multiple words"""
+print(count_info(string))  # (3, 10, 10, 20)
